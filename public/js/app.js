4d1d18a987766e28087eba1d1cd8a53cc2c42afc
Olawale <olawale.okemuyiwa@babbangona.com>
2022-02-23 10:32:31 +0100

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ form.addEventListener("submit", event => {
 
   forecastParagraph.textContent = "Loading...";
 
-  fetch(`http://localhost:3000/weather?address=${location}`)
+  fetch(`/weather?address=${location}`)
     .then(response => response.json())
     .then(data => {
       if (data.error) {
